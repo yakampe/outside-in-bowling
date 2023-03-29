c0fc26f644aa5be4472b5a7bb364ea23dd7d077f
Yanis Kampe <yanis.kampe.cv@gmail.com>
2023-03-29 18:13:37 +0100

    Revert "green"
    This reverts commit 224eb2875a9156faa98883f4a683205876871880.

--- src/main/java/dev/ykampe/BowlingGamePipeStringSplitter.java
+++ src/main/java/dev/ykampe/BowlingGamePipeStringSplitter.java
@@ -7,22 +7,10 @@ import java.util.List;
 public class BowlingGamePipeStringSplitter {
 
     public List<BowlingFrame> processGameString(String gameString) {
-        BowlingFrame bonusFrame = null;
-        if(gameString.contains("||")) {
-            bonusFrame = new BowlingFrame();
-            gameString = gameString.substring(0,gameString.indexOf("||"));
-        }
-
-
         List<BowlingFrame> bowlingFrames = new ArrayList<>();
         Arrays.asList(gameString.split("\\|")).forEach(elem -> {
             bowlingFrames.add(new BowlingFrame());
         });
-
-        if(bonusFrame != null) {
-            bowlingFrames.add(bonusFrame);
-        }
-
         return bowlingFrames;
     }
 }
