117a37020951eb72c866a5dcd1a3d264e268fd69
Yanis Kampe <yanis.kampe.cv@gmail.com>
2023-03-29 17:06:48 +0100

    acceptance test

--- src/test/java/dev/ykampe/BowlingShould.java
+++ src/test/java/dev/ykampe/BowlingShould.java
@@ -1,5 +1,22 @@
 package dev.ykampe;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BowlingShould {
-    
+
+
+    @Test
+    void process_whole_game_and_return_correct_score() {
+        //arrange
+        String inputString = "X|7/|9-|X|-8|8/|-6|X|X|X||81";
+        BowlingGame bowlingGame = new BowlingGame();
+
+        //act
+        bowlingGame.playGame(inputString);
+
+        //assert
+        assertEquals(167, bowlingGame.score());
+    }
 }
