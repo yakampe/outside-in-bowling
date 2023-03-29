7ed2d6a287cc168e4608c9ebb9b290a03504381a
Yanis Kampe <yanis.kampe.cv@gmail.com>
2023-03-29 17:44:53 +0100

    green

--- src/main/java/dev/ykampe/BowlingGame.java
+++ src/main/java/dev/ykampe/BowlingGame.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class BowlingGame {
     private final BowlingGameStringSplitter bowlingGameStringSplitter;
     private final BowlingGameFrameCalculator bowlingGameFrameCalculator;
+    private int score;
 
     public BowlingGame(BowlingGameStringSplitter bowlingGameStringSplitter, BowlingGameFrameCalculator bowlingGameFrameCalculator) {
         this.bowlingGameStringSplitter = bowlingGameStringSplitter;
@@ -12,14 +13,14 @@ public class BowlingGame {
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 
     public void playGame(String inputString) {
         List<BowlingFrame> bowlingFrames = bowlingGameStringSplitter.processGameString(inputString);
 
         bowlingFrames.forEach(bowlingFrame -> {
-            bowlingGameFrameCalculator.calculateFrameScore(bowlingFrame);
+           score += bowlingGameFrameCalculator.calculateFrameScore(bowlingFrame);
         });
     }
 }
