acba2f9711429a9fe5555c77dfbc7d00e5ee996b
Yanis Kampe <yanis.kampe.cv@gmail.com>
2023-03-29 17:19:34 +0100

    missing bowling game.java

---
+++ src/main/java/dev/ykampe/BowlingGame.java
@@ -0,0 +1,15 @@
+package dev.ykampe;
+
+public class BowlingGame {
+
+    public BowlingGame(BowlingGameStringSplitter bowlingGameStringSplitter) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
+
+    public void playGame(String inputString) {
+    }
+}
